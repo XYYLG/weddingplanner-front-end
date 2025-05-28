4e36e7b4688acbf2dd8f76e68d190404c1446081
XYYLG <547214@student.fontys.nl>
2025-05-28 16:04:22 +0200

    WIP E2E tests

--- cypress/e2e/guestManagement.cy.ts
+++ cypress/e2e/guestManagement.cy.ts
@@ -1,29 +1,34 @@
 describe('Guest Management E2E Test', () => {
-    const apiUrl = 'http://localhost:8081/guest';
-
     beforeEach(() => {
-        cy.visit('http://localhost:5173/');
+        cy.visit('http://localhost:5173/Guest');
     });
 
-    it('Moet alle gasten laden', () => {
-        cy.request(apiUrl).then((response) => {
-            expect(response.status).to.eq(200);
-            expect(response.body).to.be.an('array');
-        });
+    it('Moet een nieuwe gast kunnen toevoegen', () => {
+        // Arrange
+        cy.get('.add-guest-btn').should('be.visible').click();
+        cy.get('input[name="firstName"]').type('Test');
+        cy.get('input[name="lastName"]').type('Gebruiker');
+        cy.get('input[name="phoneNumber"]').type('0612345678');
+        cy.get('input[name="address"]').type('Straat 123, Stad');
+
+        // Act
+        cy.contains('Opslaan').click();
+
+        // Assert
+        //  cy.request('GET').its('body').should('deep.include', {
+        //    firstName: 'Test',
+        //  lastName: 'Gebruiker',
+        // phoneNumber: '0612345678',
+        // address: 'Straat 123, Stad'
+    });
+});
+
+it('Moet een gast kunnen verwijderen', () => {
+    // Arrange
+    cy.get('table tbody tr').first().within(() => {
+        cy.get('.delete-btn').click();
     });
 
-    it('Moet een nieuwe gast kunnen toevoegen', () => { cy.get('.add-guest-btn').should('be.visible').click(); cy.get('input[name="firstName"]').type('Test'); cy.get('input[name="lastName"]').type('Gebruiker'); cy.get('input[name="phoneNumber"]').type('0612345678'); cy.get('input[name="address"]').type('Straat 123, Stad'); cy.contains('Opslaan').click(); cy.request(apiUrl).its('body').should('deep.include', { firstName: 'Test', lastName: 'Gebruiker', phoneNumber: '0612345678', address: 'Straat 123, Stad' }); });
-
-
-    it('Moet een gast kunnen verwijderen', () => {
-        cy.get('table tbody tr').first().within(() => {
-            cy.get('.delete-btn').click();
-        });
-
-        cy.wait(500);
-        cy.request(apiUrl).its('body').should('not.deep.include', {
-            firstName: 'Test',
-            lastName: 'Gebruiker',
-        });
-    });
+    // Act
+    cy.wait(500);
 });
